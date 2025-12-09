c9b2f7cd24a15d5b37a793273c9143cab7170b3c
Vasil Varshilov <40742216+VasilVarshilov@users.noreply.github.com>
2025-12-09 09:09:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Crucial for GitHub Pages deployment in a subpath
+  base: './', // важно за GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
