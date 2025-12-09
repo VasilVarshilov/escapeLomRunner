8810bbf94c19006d8a7fb2eb63d8f5d83df4abc2
Vasil Varshilov <40742216+VasilVarshilov@users.noreply.github.com>
2025-12-09 12:47:57 +0200

    Update HUD.tsx

--- components/UI/HUD.tsx
+++ components/UI/HUD.tsx
@@ -279,8 +279,8 @@ export const HUD: React.FC = () => {
 
         {/* Word Collection Status */}
         {/* Force single line (flex-nowrap) and use adaptive gaps */}
-        {/* Adjusted top position to be higher on mobile (top-20) now that HUD is cleaner */}
-        <div className={`absolute top-20 md:top-24 left-1/2 transform -translate-x-1/2 flex flex-nowrap justify-center ${gapClass} max-w-full px-1 z-40`}>
+        {/* Adjusted top position to be higher on mobile (top-14) to fill space under Level indicator */}
+        <div className={`absolute top-14 md:top-24 left-1/2 transform -translate-x-1/2 flex flex-nowrap justify-center ${gapClass} max-w-full px-1 z-40`}>
             {targetWord.map((char, idx) => {
                 const isCollected = collectedLetters.includes(idx);
                 // Cycle through colors
